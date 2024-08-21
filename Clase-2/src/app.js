81a2fa053d4025ba42ad775d68c7a04f71be9a51
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-08-20 21:08:36 -0300

    se implemento mongo connect y session para un sistema de login, falta en session devolver los valores del login

---
+++ Clase-2/src/app.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import ViewRouter from './routes/views.route.js';
+import SessionsRouter from './routes/sessions.route.js';
+import { create } from 'express-handlebars';
+import { __dirname } from './utils.js';
+import mongoose from 'mongoose';
+
+const hbs = create();
+const app = express();
+
+app.engine('handlebars', hbs.engine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', ViewRouter);
+app.use('/api/sessions/', SessionsRouter);
+
+app.use(session({  
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://danielvillajuan:qpCcWENKy5dp6gRi@coderback.dkldvkl.mongodb.net/?retryWrites=true&w=majority&appName=Coderback',
+        dbName: 'users',
+        ttl: 360
+    }),
+    secret: 'C0D3RH0US3',
+    resave: true,
+    saveUninitialized: false,
+}));
+
+// app.get('/session', (req, res) => {
+//     if(!req.session.isFirst){
+//         req.session.isFirst = true;
+//         res.send('Bienvenido esta es tu primera vez')
+//     }else {        
+//         res.send('Ya estuviste por aca...')
+//     }
+// })
+
+// app.get('/deleteSession', (req, res) => {
+//     req.session.destroy((error) => {
+//         if(error) res.send('No se pudo eliminar la session')
+//         else res.send('Session eliminada')
+//     })
+// })
+
+// app.post('/login', (req, res) => {
+ 
+
+// })
+
+app.listen(8080, () => {
+    console.log('Servidor en 8080')
+})
+
+
+mongoose.connect('mongodb+srv://danielvillajuan:qpCcWENKy5dp6gRi@coderback.dkldvkl.mongodb.net/?retryWrites=true&w=majority&appName=Coderback',{dbName: 'users'})
+    .then(() => { console.log('BBDD connectada!')})
+.catch(() => {
+    console.log('Error al conectarse a la bbdd')
+})
