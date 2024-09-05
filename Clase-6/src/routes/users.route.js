6349c8c20a9ca50dfcd28f5f51016ff68ba5b272
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-09-05 20:55:50 -0300

    clase 6 terminada

--- Clase-6/src/routes/users.route.js
+++ Clase-6/src/routes/users.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { login, register } from "../controllers/user.controller.js";
-import passport from "passport";
+import { invokePassport } from "../middlewares/handlerError.js";
+import { handleAuth } from "../middlewares/handleAuth.js";
 
 const app = Router();
 
 app.post('/login', login)
 app.post('/register', register)
 
-app.get('/current', passport.authenticate('jwt', { session: false }) , (req,res) => {
+app.get('/current', invokePassport('jwt'), handleAuth('user', 'admin'), (req,res) => {
     res.send(req.user)
 })
 
