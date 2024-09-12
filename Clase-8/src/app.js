cce279415d320e4ee02e5c5f12e35aa442e7cf97
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-09-12 20:40:35 -0300

    clase 8 final

--- Clase-8/src/app.js
+++ Clase-8/src/app.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import config from './config.js'
+import { fork } from 'child_process';
 const app = express();
 
 const PORT = config.PORT;
 
-console.log(PORT)
+
+app.get('/',(req, res) =>{
+    res.send('Pagina home');
+})
+
+app.get('/suma', (req, res) => {
+    const forked = fork('./utils.js'); // fork crea un subproceso cuando se ejecute logica de utils.js
+    forked.send('Inicia el proceso') // ejecuta el evento message al hijo
+    forked.on('message',(result) => {
+        res.send('El resultado de la suma acumulada es:' + result)
+    })
+})
+
 app.listen(PORT, () => {
     console.log('server on ' + PORT)
 })
