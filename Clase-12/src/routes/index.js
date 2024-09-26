2c029c03311a4ec5f0d3fd530acdf491c3061da7
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-09-26 19:23:42 -0300

    limpiando proyecto

--- Clase-12/src/routes/index.js
+++ Clase-12/src/routes/index.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { ROUTE_PATH } from "../constants/routesPath.js";
-import ContactRouter from "./contacts.route.js";
 const app = Router()
 
-const contactRoute = new ContactRouter;
 
-app.use(ROUTE_PATH.contact, contactRoute.getRouter())
+// app.use(ROUTE_PATH.contact, contactRoute.getRouter())
 
 
 export default app
--- Clase-12/src/utils.js
+++ Clase-12/src/utils.js
@@ -1,7 +1,7 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 
 
 export const createHash = (pass) => bcrypt.hashSync(pass, bcrypt.genSaltSync(10));
@@ -12,20 +12,20 @@ const __filename = fileURLToPath(import.meta.url);
 
 export const __dirname = dirname(__filename);
 
-export const getJWTCookie = (req) => {
-    let token = null;
-    console.log(req)
-    if(req.signedCookies){
-        token = req.signedCookies['currentUser']
-    }
-    console.log(token, '<=== desde el metodo de extraccion')
-    return token
-}
+// export const getJWTCookie = (req) => {
+//     let token = null;
+//     console.log(req)
+//     if(req.signedCookies){
+//         token = req.signedCookies['currentUser']
+//     }
+//     console.log(token, '<=== desde el metodo de extraccion')
+//     return token
+// }
 
-export const generadorToken = (user) => {
-    const token = jwt.sign(user,process.env.SECRET,{ expiresIn: '1m' })
-    return token
-}
+// export const generadorToken = (user) => {
+//     const token = jwt.sign(user,process.env.SECRET,{ expiresIn: '1m' })
+//     return token
+// }
 
 export const createResponse = (res, statusCode, data) => {
     return res.status(statusCode).json({data})
